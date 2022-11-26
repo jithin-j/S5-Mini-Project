5ec4ea1ccf9a26264baed102be950187cab7b7c0
jithin-j <jithinjagadeesh1@gmail.com>
2022-11-26 21:16:49 +0530

    Added blogs feature

--- app/models.py
+++ app/models.py
@@ -5,7 +5,13 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(80), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
